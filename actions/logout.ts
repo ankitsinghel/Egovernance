b31cfba847989fc0816f4d8137c2f0d29ba2e05b
ankitsinghsinghel <singhelboyankit@gmail.com>
2025-10-23 02:49:23 +0530

    deleted actions folder

--- actions/logout.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { cookies } from "next/headers";
-
-export async function logout() {
-  const c = cookies(); // <-- this returns a Promise-like object type
-  c.set("egov_token", "", { path: "/", expires: new Date(0) }); 
-}
